81b06641fe2149a1c7f25775ad5bab5b17c99ba4
middlefgr <2giwon@gmail.com>
2020-06-05 23:15:43 +0900

    #08 ConsoleReader 커밋

---
+++ src/ConsoleReader.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class ConsoleReader(reader: BufferedReader) : AbstractReader(reader) {
+    override fun readString(message: String): Result<Pair<String, Input>> {     // 두 개의 기본 함수를 이용하여 콘솔에 표시한다
+        print("$message ")
+        return readString()
+    }
+
+    override fun readInt(message: String): Result<Pair<Int, Input>> {
+        print("$message ")
+        return readInt()
+    }
+
+    companion object {
+        operator fun invoke(): ConsoleReader = ConsoleReader(BufferedReader(InputStreamReader(System.`in`)))
+    }
+}
